1c7556ac90623f41342e4efe8d402688f3e16596
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-26 14:13:25 +0800

    Done CRUD.

--- app.js
+++ app.js
@@ -5,21 +5,82 @@ class Book extends Component {
     static template = xml /* xml */ `
         <tr>
             <th scope="row"><t t-esc="props.book.id" /></th>
-            <td><t t-esc="props.book.title" /></td>
-            <td><t t-esc="props.book.author" /></td>
-            <td><t t-esc="props.book.isbn" /></td>
             <td>
-                <button class="btn btn-sm btn-warning m-1">
+                <input t-if="state.isEditing" type="text" class="form-control" 
+                    aria-describedby="titleHelp" t-model="state.title"/>
+                <t t-if="!state.isEditing" t-esc="props.book.title" />
+            </td>
+            <td>
+                <input t-if="state.isEditing" type="text" class="form-control" 
+                    aria-describedby="titleHelp" t-model="state.author"/>
+                <t t-if="!state.isEditing" t-esc="props.book.author" />
+            </td>
+            <td>
+                <input t-if="state.isEditing" type="text" class="form-control" 
+                    aria-describedby="titleHelp" t-model="state.isbn"/>
+                <t t-if="!state.isEditing" t-esc="props.book.isbn" />
+            </td>
+            <td>
+                <button t-if="state.isEditing" class="btn btn-sm btn-info m-1"
+                    t-on-click="saveBook">
+                    <i class="bi bi-save"></i>
+                </button>
+                <button t-if="!state.isEditing" class="btn btn-sm btn-warning m-1"
+                    t-on-click="editBook">
                     <i class="bi bi-pencil"></i>
                 </button>
-                <button class="btn btn-sm btn-danger m-1">
+                <button t-if="state.isEditing" class="btn btn-sm btn-secondary m-1"
+                    t-on-click="cancelBook">
+                    <i class="bi bi-x"></i>
+                </button>
+                <button t-if="!state.isEditing" class="btn btn-sm btn-danger m-1"
+                    t-on-click="deleteBook">
                     <i class="bi bi-trash"></i>
                 </button>
             </td>
         </tr>
     `;
 
-    static props = ["book"];
+    static props = ["book", "onDelete", "onEdit"];
+
+    setup() {
+        this.state = useState({
+            isEditing: false,
+            id: this.props.book.id,
+            title: this.props.book.title,
+            author: this.props.book.author,
+            isbn: this.props.book.isbn,
+        })
+    }
+
+    editBook() {
+        this.state.isEditing = true;
+    }
+
+    saveBook() {
+        this.state.isEditing = false;
+
+        // validate fields
+        if(!this.state.title || !this.state.author || !this.state.isbn) {
+            alert("All fields are required!");
+            this.state.isEditing = true;
+            return
+        } else {
+            this.props.onEdit(this.state);
+        }
+
+    }
+
+    deleteBook() {
+        this.props.onDelete(this.props.book);
+    }
+
+    cancelBook() {
+        this.state.isEditing = false;
+        this.state.title = this.props.book.title
+        this.state.author = this.props.book.author
+        this.state.isbn = this.props.book.isbn
+    }
 }
 
 // Root Component
@@ -43,7 +104,8 @@ class Root extends Component {
                         aria-describedby="isbnHelp" t-model="state.isbn"/>
                 </div>
                 <div class="mb-3 text-end">
-                    <button type="submit" class="btn btn-primary text-right" t-on-click="addBook">
+                    <button type="submit" class="btn btn-primary text-right" 
+                        t-on-click="addBook">
                         <i class="bi bi-plus"></i> Add Book
                     </button>
                 </div>
@@ -62,7 +124,8 @@ class Root extends Component {
             </thead>
             <tbody>
                 <t t-foreach="books" t-as="book" t-key="book.id">
-                    <Book book="book" />
+                    <Book book="book" onDelete.bind="deleteBook"
+                        onEdit.bind="editBook" />
                 </t>
             </tbody>
         </table>
@@ -75,14 +138,17 @@ class Root extends Component {
             title: "",
             author: "",
             isbn: "",
+            isEditing: false,
         });
 
         this.books = useState([]);
     }
 
+    // Add book
     addBook(event) {
+        // Prevent reloading when submitting the form
         event.preventDefault();
-        console.log("Add book CLICKED!")
+        
         // validate fields
         if(!this.state.title || !this.state.author || !this.state.isbn) {
             alert("All fields are required!");
@@ -104,6 +170,18 @@ class Root extends Component {
         this.state.title = ""
         this.state.author = ""
         this.state.isbn = ""
+    }
+
+    // Edit book
+    editBook(book) {
+        const index = this.books.findIndex((b) => b.id === book.id);
+        this.books.splice(index, 1, book)
+    }
+
+    // Delete book
+    deleteBook(book) {
+        const index = this.books.findIndex(b => b.id === book.id);
+        this.books.splice(index, 1);
     }
 }
 
